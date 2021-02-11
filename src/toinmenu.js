050c19052566faa9c0620b8a7bd8e4a87a52bd8e
marcosmods <78054628+marcosmods@users.noreply.github.com>
2021-02-11 13:47:29 -0300

    update toinmenu.js

--- src/toinmenu.js
+++ src/toinmenu.js
@@ -1,11 +1,11 @@
 const toinmenu = (prefix, pushname) => {
     return `◪ *comandos do 𖡜ꦽ̸ོ˚￫𝑲𝐈𝐓𝐎𖥨ํ∘̥⃟⸽⃟🌹 *
-    │
-    ├─ ❏ ${prefix}setprefix
-    ├─ ❏ ${prefix}block
-    ├─ ❏ ${prefix}bc
-    ├─ ❏ ${prefix}bcgc
-    └─ ❏ ${prefix}clearall`
+    
+▻${prefix}setprefix
+▻${prefix}block
+▻${prefix}bc
+▻${prefix}bcgc
+▻${prefix}clearall`
 
 }
 
