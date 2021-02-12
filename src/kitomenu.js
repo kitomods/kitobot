0495b3bd7c341d83e69e9343272f629e49e5ef82
marcosmods <78054628+marcosmods@users.noreply.github.com>
2021-02-12 14:42:22 -0300

    Add files via upload

---
+++ src/kitomenu.js
@@ -0,0 +1,12 @@
+const kitomenu = (prefix, pushname) => {
+    return `◪ *comandos do 𖡜ꦽ̸ོ˚￫𝑲𝐈𝐓𝐎𖥨ํ∘̥⃟⸽⃟🌹 *
+    
+▻${prefix}setprefix
+▻${prefix}block
+▻${prefix}bc
+▻${prefix}bcgc
+▻${prefix}clearall`
+
+}
+
+exports.kitomenu = kitomenu
