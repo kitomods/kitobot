5e8d28867f82c46cf52149f6dccbae4a27f86217
marcosmods <78054628+marcosmods@users.noreply.github.com>
2021-02-14 01:30:46 -0300

    update index.js

--- index.js
+++ index.js
@@ -284,7 +284,7 @@ async function starts() {
 			const isGroupAdmins = groupAdmins.includes(sender) || false
 			const isWelkom = isGroup ? welkom.includes(from) : false
 			const isNsfw = isGroup ? nsfw.includes(from) : false
-			const isSimi = isGroup ? samih.includes(from) : false 
+			const isSimi = isGroup ? samih.includes(from) : false
 			const isOwner = ownerNumber.includes(sender)
                         const isUser = user.includes(sender)
                         const isLevelingOn = isGroup ? _leveling.includes(groupId) : false
@@ -465,6 +465,13 @@ async function starts() {
 			                if (!isUser) return reply(mess.only.daftarB)
 					anu = await fetchJson(`https://arugaz.my.id/api/wikien?q=${tels}`, {method: 'get'})
 					reply(anu.result)
+					break
+             case 'simi':
+					if (args.length < 1) return reply('Onde está o texto?')
+					teks = body.slice(5)
+					anu = await simih(teks) //fetchJson(`https://mhankbarbars.herokuapp.com/api/samisami?text=${teks}`, {method: 'get'})
+					//if (anu.error) return reply('Simi ga tau kak')
+					reply(anu)
 					break
              case 'simih':
 				if (isBanned) return reply(mess.only.benned)    
