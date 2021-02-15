d39f6b199e2494575f4ec86ba7637d6d7e22ddf3
marcosmods <78054628+marcosmods@users.noreply.github.com>
2021-02-15 17:49:25 -0300

    update menulinks.js

--- src/menulinks.js
+++ src/menulinks.js
@@ -2,9 +2,36 @@ const menulinks = (prefix, pushname) => {
  return `
 🜲𝐌𝐄𝐍𝐔 𝐃𝐄 𝐋𝐈𝐍𝐊𝐒🜲
   
-                 
+               🜲𝗔𝗣𝗣𝗦🜲
 𒆕𝗠𝗜𝗡𝗘𝗖𝗥𝗔𝗙𝗧
 https://www.mediafire.com/file/khqflpzr02u721m/minecraft-1-16-201-01-xbox-servers.apk/file
+
+𒆕𝗦𝗣𝗢𝗧𝗙𝗬 𝗣𝗥𝗢
+https://www.mediafire.com/file/1sthqzz9d99pc4o/SPOTFY+PREMIUM+ATUALIZADO+2021+%C2%A9007Apk's.apk/file
+
+              🜲𝗜𝗠𝗨𝗡𝗘𝗦 𝗣𝗥𝗜𝗠𝗔𝗥𝗜𝗢𝗦🜲
+𒆕𝗕𝗔𝗦𝗘=⏤͟͟͞͞𝐕𝐈𝐍𝐈𝐙𝐈𝐍⸸𝐓𝐔𝚴𝚴𝚵𝐃•᭄ᷟ
+https://www.mediafire.com/file/yv9ga2sy3tftvs5/%25E2%258F%25A4%25CD%259F%25CD%259E%25CD%259F%25CD%259E%25F0%259D%2590%2595%25F0%259D%2590%2588%25F0%259D%2590%258D%25F0%259D%2590%2588%25F0%259D%2590%2599%25F0%259D%2590%2588%25F0%259D%2590%258D%25E2%25B8%25B8%25F0%259D%2590%2593%25F0%259D%2590%2594%25F0%259D%259A%25B4%25F0%259D%259A%25B4%25F0%259D%259A%25B5%25F0%259D%2590%2583%25E2%2580%25A2%25E1%25AD%2584%25E1%25B7%259F.apk/file
+
+𒆕༒︎𝗕𝗜𝗟𝗟𝗦_𝗩8༒︎
+https://www.mediafire.com/file/6hczfp3pf828gzv/%25E0%25BC%2592%25EF%25B8%258E%25F0%259D%2597%2595%25F0%259D%2597%259C%25F0%259D%2597%259F%25F0%259D%2597%259F%25F0%259D%2597%25A6_%25F0%259D%2597%25A98%25E0%25BC%2592%25EF%25B8%258E_r.apk/file
+
+𒆕↯𝐈𝐌𝚯𝐑𝐓∆𝐋⛧𝐗𝐗𝐕ᬏ᭄
+http://www.mediafire.com/file/qfs7exjuwrp6qll/↯𝐈𝐌𝚯𝐑𝐓∆𝐋⛧𝐗𝐗𝐕ᬏ᭄.apk/file
+
+𒆕YGGDRASIL - 𝐓𝐔𝐍𝛯𝛯𝐃
+https://www.mediafire.com/file/47jlw3kpr391j7d/YGGDRASIL_-_%25F0%259D%2590%2593%25F0%259D%2590%2594%25F0%259D%2590%258D%25F0%259D%259B%25AF%25F0%259D%259B%25AF%25F0%259D%2590%2583.apk/file
+
+
+              🜲𝗜𝗠𝗨𝗡𝗘𝗦 𝗦𝗘𝗖𝗨𝗡𝗗𝗔𝗥𝗜𝗢𝗦🜲
+𒆕⏤͟͟͞͞𝑵𝜟𝛩𝑲𝑰 ⛧ 𝑾𝜟 ⛧ 𝐕7 ⸸
+https://www.mediafire.com/file/ozp19sott8jyc84/%25E2%258F%25A4%25CD%259F%25CD%259E%25CD%259F%25CD%259E%25F0%259D%2591%25B5%25F0%259D%259C%259F%25F0%259D%259B%25A9%25F0%259D%2591%25B2%25F0%259D%2591%25B0_%25E2%259B%25A7_%25F0%259D%2591%25BE%25F0%259D%259C%259F_%25E2%259B%25A7_%25F0%259D%2590%25957_%25E2%25B8%25B8.apk/file
+
+𒆕𝗦𝗛𝗔𝗥𝗜𝗡𝗚𝗔𝗡 𝗩𝟰
+http://www.mediafire.com/file/dh341g1hjgnj89e/SHARINGAN+V4+V3.apk/file
+
+𒆕• 𝐃𝐄𝐀𝐓𝐇 𝐖𝐀 𝐕𝟓 𝐓𝐔𝐍𝐍𝐄𝐃 •
+https://mega.nz/file/bs0DzQBI#JmUHB4SNmKgkzWZZHjN1Vm3YRTQ-qpLJjuxNaIV0MhQ
 
  `
 
