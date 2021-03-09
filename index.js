e41afa1be4011f7f4bd280092e2fa82c0135eb8a
kitomods <78054628+kitomods@users.noreply.github.com>
2021-03-09 09:03:47 -0300

    update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const { othermenu } require('./database/menu/othermenu')*/
 /******LOAD OF VCARD INPUT******/
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:kito\n' // full name
+            + 'FN:@ \n' // full name
             + 'ORG:Owner Bot;\n' // the organization of the contact
             + 'TEL;type=CELL;type=VOICE;waid=556296638900:+55 (28) 99903-0751\n' // ID do WhatsApp + número de telefone
             + 'END:VCARD'
